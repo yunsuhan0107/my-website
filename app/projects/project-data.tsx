12058c003af7fb807b0fa05ae612ba981a72812f
Yunsu Han <46840483+yunsuhan0107@users.noreply.github.com>
2024-12-13 19:19:46 +0900

    Update project-data.tsx
    Added project url

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -11,6 +11,6 @@ export const projects: Project[] = [
     year: 2019,
     description:
       "A software-focused project of a system that differentiates images of a person drowning and swimming for maritime safety.",
-    url: "",
+    url: "https://docs.google.com/document/d/e/2PACX-1vTZ8C0WeSUXQcvBS0j2MUVEImRg_SnJcksSh82RJUN_MEZnCef_oKSwHIZOtl0PCg/pub",
   },
 ];
