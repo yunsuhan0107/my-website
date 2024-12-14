039198639e86166be80f2125d70afc25e37eba65
Yunsu Han <46840483+yunsuhan0107@users.noreply.github.com>
2024-12-14 13:36:50 +0900

    Update page.tsx
    Removed "Welcome."

--- app/page.tsx
+++ app/page.tsx
@@ -72,7 +72,7 @@ export default function Page() {
       </a>
 
       <h1 className="mb-8 text-2xl font-medium tracking-tight">
-        {randomHello || "Welcome"}.
+        {randomHello}.
       </h1>
 
       <div className="prose prose-neutral dark:prose-invert">
