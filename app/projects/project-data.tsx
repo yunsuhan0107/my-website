e52024b9df52d10f86bb9405cda3217f08e84add
Yunsu Han <yunsuhan00107@gmail.com>
2025-10-08 00:19:03 -0500

    changed project files to pdf

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -11,13 +11,13 @@ export const projects: Project[] = [
     year: 2022,
     description:
         "Using ESP-32 Camera and Arduino board with ResNet50 Architecture, the trash can detects and classifies the trash's category and transfers it to the bin accordingly.",
-    url: "https://docs.google.com/document/d/1-w4rcG7msn8TzT2jdoI4S8_pfglgfBkPopTKYTUxhxg/edit?usp=sharing"
+    url: "/pdf/project2.pdf"
   },
   {
     title: "Comparison of Drowning People Detection Systems for Water Safety",
     year: 2019,
     description:
       "A software-focused project of a system that differentiates images of a person drowning and swimming for maritime safety.",
-    url: "https://docs.google.com/document/d/19udQhYzRlnVK-7ee5Xki1MTNUNUl0eMe/edit?usp=sharing&ouid=117489009999024997956&rtpof=true&sd=true",
+    url: "/pdf/project1.pdf",
   }
 ];
